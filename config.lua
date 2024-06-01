d83e52065f31782c4930032acf0af8997b3ed361
Larry! [EQ] <enzoack@icloud.com>
2024-06-01 14:55:38 +0200

    dev commit

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.Colors = {
+    ["staff"] = {255, 165, 0}, -- Orange
+    ["admin"] = {255, 0, 0} -- Rouge
+}
+
+Config.Emojis = {
+    ["staff"] = "⚡​", 
+    ["admin"] = "💫​​" 
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+author 'Larry2_0'
+
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server/server.lua'
+}
